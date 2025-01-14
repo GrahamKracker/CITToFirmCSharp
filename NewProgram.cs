884ec20e26f918ee8d074049edb4112a216f8380
GrahamKracker <gjguzynski@gmail.com>
2025-01-14 14:19:23 -0500

    start of new project

---
+++ NewProgram.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace CITToFirmCSharp;
+
+public class NewProgram
+{
+    private static readonly HashSet<string> _itemIds = new();
+
+    private async static Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        var response = JsonSerializer.Deserialize<JsonObject[]>(await client.GetStringAsync("https://api.hypixel.net/v2/resources/skyblock/items"));
+        foreach (var item in response)
+        {
+            _itemIds.Add(item["id"].ToString());
+        }
+        await new NewProgram().Run();
+    }
+
+    public async Task Run()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
